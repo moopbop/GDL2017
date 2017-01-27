ad51f0667d7f9fd36ed786d04d0f5679f1550483
Jacob Mangia <tucker2134@gmail.com>
2017-01-26 23:08:08 -0500

    Updated Camera Control
    Mouse hidden and locked to screen

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -7,5 +7,14 @@ public class Globals : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
 	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
 }
