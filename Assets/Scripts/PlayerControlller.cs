494a99d776333b2db26ef1c5e8d8513a082fb2a3
moopbop <moopbop@gmail.com>
2017-01-26 22:17:20 -0500

    Updated player to use physics

--- Assets/Scripts/PlayerControlller.cs
+++ Assets/Scripts/PlayerControlller.cs
@@ -7,7 +7,9 @@ public class PlayerControlller : MonoBehaviour {
     public float MoveSpeed = 10;
     public float TurnSpeed = 60;
 
-    float TurnModifier = 1;
+    public float TurnModifier = 1;
+    bool UseTurnModifier = false;
+    
 
 	// Use this for initialization
 	void Start () {
@@ -17,14 +19,27 @@ public class PlayerControlller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey(KeyCode.LeftShift))
-            TurnModifier = 2;
+            UseTurnModifier = true;
         else
-            TurnModifier = 1;
+            UseTurnModifier = false;
 
-        float hMove = Input.GetAxis("Horizontal") * (TurnSpeed * TurnModifier) * Time.deltaTime;
+        float hMove = 0.0F;
+
+        if (UseTurnModifier)
+            hMove = Input.GetAxis("Horizontal") * (TurnSpeed * TurnModifier) * Time.deltaTime;
+        else
+            hMove = Input.GetAxis("Horizontal") * TurnSpeed * Time.deltaTime;
+
         float vMove = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
 
-        transform.Rotate(0, hMove, 0);
-        transform.Translate(0, 0, vMove);
+        //transform.Rotate(0, hMove, 0);
+        //transform.Translate(0, 0, vMove);
+        gameObject.GetComponent<Rigidbody>().AddRelativeTorque(0, hMove, 0);
+        gameObject.GetComponent<Rigidbody>().AddRelativeForce(0, 0, vMove);
 	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+    }
 }
