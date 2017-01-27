c2c4c3d4117b12cc7f7797653baad9ecb0ec5d4a
Jacob Mangia <tucker2134@gmail.com>
2017-01-26 22:49:59 -0500

    Camera Control
    Added Camer Control

--- Assets/Scripts/DungeonCrawlerCamera.cs
+++ Assets/Scripts/DungeonCrawlerCamera.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class DungeonCrawlerCamera : MonoBehaviour {
 
     public GameObject Target;
-    Vector3 Offset;
-    public float Damping = 1;
+    Vector3 offset;
+    public float Damping = 1f;
+    public float speed = 5f;
 
 	// Use this for initialization
 	void Start () {
-        Offset = transform.position - Target.transform.position;
+        offset = transform.position - Target.transform.position;
 	}
 	
 	void LateUpdate () {
-        Vector3 desiredPosition = Target.transform.position + Offset;
-        Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * Damping);
-        transform.position = position;
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * speed, Vector3.up) * offset;
+        transform.position = Target.transform.position + offset;
         transform.LookAt(Target.transform.position);
-        
+        //Vector3 desiredPosition = Target.transform.position + Offset;
+        //Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * Damping);
+        //transform.position = position;
+        //transform.LookAt(Target.transform.position);
+        //if (Input.GetMouseButton(1))
+        //{
+            //transform.RotateAround(Target.transform.position, Vector3.up, Input.GetAxis("Mouse X") * speed);
+            //transform.RotateAround(Target.transform.position, Vector3.right, -Input.GetAxis("Mouse Y") * speed);
+
+        //}
         
 	}
 }
