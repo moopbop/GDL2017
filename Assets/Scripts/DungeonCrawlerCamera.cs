435e36bc7c0e724c1f70253d22ac039132f2c85e
moopbop <moopbop@gmail.com>
2017-01-26 20:58:09 -0500

    Implemented third-person camera + backpack

---
+++ Assets/Scripts/DungeonCrawlerCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonCrawlerCamera : MonoBehaviour {
+
+    public GameObject Target;
+    Vector3 Offset;
+    public float Damping = 1;
+
+	// Use this for initialization
+	void Start () {
+        Offset = transform.position - Target.transform.position;
+	}
+	
+	void LateUpdate () {
+        Vector3 desiredPosition = Target.transform.position + Offset;
+        Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * Damping);
+        transform.position = position;
+        transform.LookAt(Target.transform.position);
+        
+        
+	}
+}
---
+++ Assets/Scripts/PlayerControlller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlller : MonoBehaviour {
+
+    public float MoveSpeed = 10;
+    public float TurnSpeed = 60;
+
+    float TurnModifier = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftShift))
+            TurnModifier = 2;
+        else
+            TurnModifier = 1;
+
+        float hMove = Input.GetAxis("Horizontal") * (TurnSpeed * TurnModifier) * Time.deltaTime;
+        float vMove = Input.GetAxis("Vertical") * MoveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, hMove, 0);
+        transform.Translate(0, 0, vMove);
+	}
+}
