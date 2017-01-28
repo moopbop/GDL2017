b9c42d09ad1ca471b3b7c87b63c11d21fe81ebd9
moopbop <moopbop@gmail.com>
2017-01-27 22:30:30 -0500

    Commit before switching to master to check coffee

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour {
 
-    public float moveSpeed = 100f;
-    public float turnSpeed = 40f;
+    //public float moveSpeed = 100f;
+    //public float turnSpeed = 40f;
     public Vector3 centerOfMass;
-
+    public float gravity;
     public float turnModifier = 1.5f;
+    public float floatDistance = 5;
 
     public GameObject Pilot;
 
-
-    bool useTurnModifier = false;
+    float moveSpeed;
+    float turnSpeed;
+    bool useTurnModifier;
     float hMove = 0.0f;
     float vMove = 0.0f;
     Quaternion initialRotation;
     Vector3 initialPosition;
+    bool useGravity;
+    Ray gravityCheckRay;
 
 	// Use this for initialization
 	void Start () {
+        useTurnModifier = false;
+        useGravity = true;
         initialRotation = gameObject.transform.rotation;
         initialPosition = gameObject.transform.position;
         GetComponent<Rigidbody>().centerOfMass = centerOfMass;
+
+        moveSpeed = GetComponent<Rigidbody>().mass * 5000;
+        turnSpeed = GetComponent<Rigidbody>().mass * 2500;
+
+        gravityCheckRay.origin = gameObject.transform.position;
+        //gravityCheckRay.direction.
 	}
 
     void ResetPosition()
@@ -49,7 +61,7 @@ public class CarController : MonoBehaviour {
 
         vMove = Input.GetAxis("hInput") * moveSpeed * Time.deltaTime;
 
-        
+        Debug.Log(useGravity);
     }
 
     void FixedUpdate()
@@ -59,6 +71,25 @@ public class CarController : MonoBehaviour {
         else
             gameObject.GetComponent<Rigidbody>().AddTorque(0, hMove, 0);
 
-        gameObject.GetComponent<Rigidbody>().AddRelativeForce(0, 0, vMove);
+        Vector3 force = new Vector3(0, 0, vMove);
+        if (useGravity)
+            force.y = -gravity;
+
+        gameObject.GetComponent<Rigidbody>().AddRelativeForce(force);
     }
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    useGravity = false;
+    //}
+
+    //void OnCollisionExit(Collision collision)
+    //{
+    //    useGravity = true;
+    //}
+
+    //void OnCollisionStay(Collision collision)
+    //{
+    //    useGravity = false;
+    //}
 }
