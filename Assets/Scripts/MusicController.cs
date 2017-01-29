b7b7f0430b2b17626b65e86c0c01ec198701da0c
moopbop <moopbop@gmail.com>
2017-01-29 16:54:46 -0500

    Music completed

--- Assets/Scripts/MusicController.cs
+++ Assets/Scripts/MusicController.cs
@@ -8,29 +8,41 @@ public class MusicController : MonoBehaviour {
     public AudioMixerSnapshot lowTime;
     public AudioMixerSnapshot climax;
     public AudioMixerSnapshot buildup;
-    public float bpm = 108;
+    public float bpm = 106;
 
     private float fadeTime;
     private float quarterNote;
+    private float fastFadeTime;
 
 	// Use this for initialization
 	void Start () {
         quarterNote = 60 / bpm;
-        fadeTime = quarterNote * 2;
+        fadeTime = quarterNote;
+        //fastFadeTime = quarterNote;
+
+        lowTime.TransitionTo(0);
 	}
 
 
-    void OnTriggerExit(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             buildup.TransitionTo(fadeTime);
         }
+        else if (other.CompareTag("Car") && other.gameObject.GetComponent<CarController>().Pilot != null)
+        {
+            buildup.TransitionTo(fadeTime);
+        }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
+            lowTime.TransitionTo(fadeTime);
+        }
+        else if (other.CompareTag("Car") && other.gameObject.GetComponent<CarController>().Pilot != null)
         {
             lowTime.TransitionTo(fadeTime);
         }
@@ -40,7 +52,7 @@ public class MusicController : MonoBehaviour {
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            climax.TransitionTo(fadeTime);
+            climax.TransitionTo(0);
         }
     }
 
