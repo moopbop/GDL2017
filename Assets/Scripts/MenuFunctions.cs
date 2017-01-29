9c664e1d74cc196f483dc231d56718589a9ac9b9
Jacob Mangia <tucker2134@gmail.com>
2017-01-28 22:53:42 -0500

    Main Menu Updates
    Added Story Section

--- Assets/Scripts/MenuFunctions.cs
+++ Assets/Scripts/MenuFunctions.cs
@@ -7,11 +7,20 @@ public class MenuFunctions : MonoBehaviour {
 
     private void Start()
     {
-
+        if (this.gameObject != GameObject.Find("MainMenu"))
+        {
+            this.gameObject.SetActive(false);
+        }
     }
 
     public void ClickedStart()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    public void changeMenu(GameObject menu)
+    {
+        menu.SetActive(true);
+        this.gameObject.SetActive(false);
     }
 }
